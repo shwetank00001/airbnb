4aee2dd22ea5b61c2b4e0cd69ccc5b988c9bc4a7
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-01-07 04:58:31 +0530

    basic prop setup done in AIRBNB

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Nav from './ComponentsRev/Nav'
 import Hero from './ComponentsRev/Hero'
 import Card from './ComponentsRev/Card'
 
+import Katie from './images/katie.png'
+
 
 
 const App = () => {
@@ -10,7 +12,15 @@ const App = () => {
     <div>
       <Nav />
       <Hero />
-      <Card />
+
+      <Card
+        pic = {Katie}
+        rating = "5.0"
+        reviewCount = {6}
+        title = "Win at life with Shwetank"
+        price = {136}
+        country = "India"
+         />
     </div>
   )
 }
--- src/ComponentsRev/Card.jsx
+++ src/ComponentsRev/Card.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import './card.css'
 import Katie from '../images/katie.png'
+import Star from '../images/star-logo.png'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
-        <img src={Katie} alt='katie'/>
+        <img className='cardImg' src= {props.pic} alt='katie'/>
+
+        <div>
+            <div className='ratings'>
+                <img className='starImg' src={Star} alt='katie'/>
+                <p>{props.rating} ({props.reviewCount}) • {props.country}</p>
+            </div> 
+            <p>{props.title}</p>
+            <p><b>From ${props.price}</b>/person</p>
+        </div>
+
+        
     </div>
   )
 }
